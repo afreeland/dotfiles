3888675d2d2189575f7e35152e243c0c2b277d97
Aaron Freeland <aaron.freeland@corelight.com>
2026-02-17 16:29:32 -0500

    hurl treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter.git" })
 vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter-textobjects.git" })
 
 require('nvim-treesitter.configs').setup({
-    ensure_installed = { 'go', 'lua', 'python', 'javascript', 'typescript' },
+    ensure_installed = { 'go', 'lua', 'python', 'javascript', 'typescript', 'hurl' },
     highlight = {
         enable = true,
     },
